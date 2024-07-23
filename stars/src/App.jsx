3955bec6d7438721d10ba9d138e69d55be8d03d8
Yuval Lande <yuvalande@gmail.com>
2024-07-23 18:40:46 +0300

    Updated stars colors and positions, and incresed zoom

--- stars/src/App.jsx
+++ stars/src/App.jsx
@@ -8,32 +8,32 @@ const SCALE_FACTOR = 2; // Adjust this factor to control the spread
 const X_STRETCH_FACTOR = 2; // Factor to stretch x-coordinates
 
 const fixedPositions = [
-  { name: "Ree", position: [-0.8 * SCALE_FACTOR * X_STRETCH_FACTOR, 0.8 * SCALE_FACTOR, -0.5 * SCALE_FACTOR] },
-  { name: "Sev", position: [-0.5 * SCALE_FACTOR * X_STRETCH_FACTOR, 0.9 * SCALE_FACTOR, 0.3 * SCALE_FACTOR] },
-  { name: "Enti", position: [-0.2 * SCALE_FACTOR * X_STRETCH_FACTOR, 0.9 * SCALE_FACTOR, 0.7 * SCALE_FACTOR] },
-  { name: "Tahun", position: [0.3 * SCALE_FACTOR * X_STRETCH_FACTOR, 0.9 * SCALE_FACTOR, -0.3 * SCALE_FACTOR] },
-  { name: "Thurt", position: [0.7 * SCALE_FACTOR * X_STRETCH_FACTOR, 0.9 * SCALE_FACTOR, 0.8 * SCALE_FACTOR] },
-  { name: "Phorr", position: [0.8 * SCALE_FACTOR * X_STRETCH_FACTOR, 0.5 * SCALE_FACTOR, -0.6 * SCALE_FACTOR] },
-  { name: "Wonn", position: [-0.9 * SCALE_FACTOR * X_STRETCH_FACTOR, 0.6 * SCALE_FACTOR, 0.2 * SCALE_FACTOR] },
-  { name: "AT-2", position: [0.1 * SCALE_FACTOR * X_STRETCH_FACTOR, 0.7 * SCALE_FACTOR, -0.1 * SCALE_FACTOR] },
-  { name: "Toonyne", position: [-0.6 * SCALE_FACTOR * X_STRETCH_FACTOR, 0.2 * SCALE_FACTOR, 0.9 * SCALE_FACTOR] },
-  { name: "Kpyr", position: [0 * SCALE_FACTOR * X_STRETCH_FACTOR, 0.2 * SCALE_FACTOR, -0.8 * SCALE_FACTOR] },
-  { name: "Sirius", position: [0.4 * SCALE_FACTOR * X_STRETCH_FACTOR, 0.1 * SCALE_FACTOR, 0.5 * SCALE_FACTOR] },
-  { name: "Vega", position: [0.8 * SCALE_FACTOR * X_STRETCH_FACTOR, 0.2 * SCALE_FACTOR, -0.4 * SCALE_FACTOR] },
-  { name: "Aytee", position: [-0.7 * SCALE_FACTOR * X_STRETCH_FACTOR, -0.1 * SCALE_FACTOR, 0.6 * SCALE_FACTOR] },
-  { name: "NT-9", position: [-0.2 * SCALE_FACTOR * X_STRETCH_FACTOR, -0.1 * SCALE_FACTOR, -0.2 * SCALE_FACTOR] },
-  { name: "Dreddand", position: [0.1 * SCALE_FACTOR * X_STRETCH_FACTOR, -0.2 * SCALE_FACTOR, 0.4 * SCALE_FACTOR] },
-  { name: "Forti", position: [0.5 * SCALE_FACTOR * X_STRETCH_FACTOR, -0.1 * SCALE_FACTOR, -0.9 * SCALE_FACTOR] },
-  { name: "Minuss", position: [0.8 * SCALE_FACTOR * X_STRETCH_FACTOR, -0.3 * SCALE_FACTOR, 0.1 * SCALE_FACTOR] },
-  { name: "Hun", position: [-0.9 * SCALE_FACTOR * X_STRETCH_FACTOR, -0.8 * SCALE_FACTOR, -0.7 * SCALE_FACTOR] },
-  // { name: "Phives", position: [-0.3 * SCALE_FACTOR * X_STRETCH_FACTOR, -0.6 * SCALE_FACTOR, 0 * SCALE_FACTOR] },
-  { name: "Nyne", position: [0.2 * SCALE_FACTOR * X_STRETCH_FACTOR, -0.5 * SCALE_FACTOR, 0.2 * SCALE_FACTOR] },
-  { name: "Xiero", position: [0.6 * SCALE_FACTOR * X_STRETCH_FACTOR, -0.7 * SCALE_FACTOR, -0.5 * SCALE_FACTOR] },
-  { name: "Sikstee", position: [-0.4 * SCALE_FACTOR * X_STRETCH_FACTOR, 0.5 * SCALE_FACTOR, -0.4 * SCALE_FACTOR] },
-  { name: "Betelgeuse", position: [0.5 * SCALE_FACTOR * X_STRETCH_FACTOR, -0.4 * SCALE_FACTOR, 0.6 * SCALE_FACTOR] },
-  { name: "Tiwon", position: [0.3 * SCALE_FACTOR * X_STRETCH_FACTOR, 0.6 * SCALE_FACTOR, 0.4 * SCALE_FACTOR] },
-  { name: "Altair", position: [-0.6 * SCALE_FACTOR * X_STRETCH_FACTOR, -0.3 * SCALE_FACTOR, -0.3 * SCALE_FACTOR] },
-  { name: "Procyon", position: [-0.1 * SCALE_FACTOR * X_STRETCH_FACTOR, 0.4 * SCALE_FACTOR, -0.7 * SCALE_FACTOR] },
+  { name: "Ree", position: [-0.67 * SCALE_FACTOR * X_STRETCH_FACTOR, 0.51 * SCALE_FACTOR, -0.5 * SCALE_FACTOR] },
+  { name: "Sev", position: [-0.51 * SCALE_FACTOR * X_STRETCH_FACTOR, 0.76 * SCALE_FACTOR, 0.3 * SCALE_FACTOR] },
+  { name: "Enti", position: [-0.23 * SCALE_FACTOR * X_STRETCH_FACTOR, 0.9 * SCALE_FACTOR, 0.7 * SCALE_FACTOR] },
+  { name: "Tahun", position: [0.26 * SCALE_FACTOR * X_STRETCH_FACTOR, 0.65 * SCALE_FACTOR, -0.3 * SCALE_FACTOR] },
+  { name: "Thurt", position: [0.83 * SCALE_FACTOR * X_STRETCH_FACTOR, 0.95 * SCALE_FACTOR, 0.8 * SCALE_FACTOR] },
+  { name: "Phorr", position: [0.65 * SCALE_FACTOR * X_STRETCH_FACTOR, 0.3 * SCALE_FACTOR, -0.6 * SCALE_FACTOR] },
+  { name: "Wonn", position: [-0.88 * SCALE_FACTOR * X_STRETCH_FACTOR, 0.45 * SCALE_FACTOR, 0.2 * SCALE_FACTOR] },
+  { name: "AT-2", position: [0.09 * SCALE_FACTOR * X_STRETCH_FACTOR, 0.5 * SCALE_FACTOR, -0.1 * SCALE_FACTOR] },
+  { name: "Toonyne", position: [-0.73 * SCALE_FACTOR * X_STRETCH_FACTOR, 0.1 * SCALE_FACTOR, 0.9 * SCALE_FACTOR] },
+  { name: "Kpyr", position: [0.01 * SCALE_FACTOR * X_STRETCH_FACTOR, 0 * SCALE_FACTOR, -0.8 * SCALE_FACTOR] },
+  { name: "Sirius", position: [0.43 * SCALE_FACTOR * X_STRETCH_FACTOR, 0 * SCALE_FACTOR, 0.5 * SCALE_FACTOR] },
+  { name: "Vega", position: [0.68 * SCALE_FACTOR * X_STRETCH_FACTOR, 0.05 * SCALE_FACTOR, -0.4 * SCALE_FACTOR] },
+  { name: "Aytee", position: [-0.77 * SCALE_FACTOR * X_STRETCH_FACTOR, -0.24 * SCALE_FACTOR, 0.6 * SCALE_FACTOR] },
+  { name: "NT-9", position: [-0.17 * SCALE_FACTOR * X_STRETCH_FACTOR, -0.21 * SCALE_FACTOR, -0.2 * SCALE_FACTOR] },
+  { name: "Dreddand", position: [0.11 * SCALE_FACTOR * X_STRETCH_FACTOR, -0.32 * SCALE_FACTOR, 0.4 * SCALE_FACTOR] },
+  { name: "Forti", position: [0.4 * SCALE_FACTOR * X_STRETCH_FACTOR, -0.2 * SCALE_FACTOR, -0.9 * SCALE_FACTOR] },
+  { name: "Minuss", position: [0.77 * SCALE_FACTOR * X_STRETCH_FACTOR, -0.4 * SCALE_FACTOR, 0.1 * SCALE_FACTOR] },
+  { name: "Hun", position: [-0.71 * SCALE_FACTOR * X_STRETCH_FACTOR, -0.8 * SCALE_FACTOR, -0.7 * SCALE_FACTOR] },
+  // { name: "Phives", position: [-0.3 * SCALE_FACTOR * X_STRETCH_7ACTOR, -0.6 * SCALE_FACTOR, 0 * SCALE_FACTOR] },
+  { name: "Nyne", position: [0.2 * SCALE_FACTOR * X_STRETCH_FACTOR, -0.6 * SCALE_FACTOR, 0.2 * SCALE_FACTOR] },
+  { name: "Xiero", position: [0.5 * SCALE_FACTOR * X_STRETCH_FACTOR, -0.7 * SCALE_FACTOR, -0.5 * SCALE_FACTOR] },
+  { name: "Sikstee", position: [-0.34 * SCALE_FACTOR * X_STRETCH_FACTOR, 0.29 * SCALE_FACTOR, -0.4 * SCALE_FACTOR] },
+  { name: "Betelgeuse", position: [0.55 * SCALE_FACTOR * X_STRETCH_FACTOR, -0.55 * SCALE_FACTOR, 0.6 * SCALE_FACTOR] },
+  { name: "Tiwon", position: [0.31 * SCALE_FACTOR * X_STRETCH_FACTOR, 0.5 * SCALE_FACTOR, 0.4 * SCALE_FACTOR] },
+  { name: "Altair", position: [-0.52 * SCALE_FACTOR * X_STRETCH_FACTOR, -0.4 * SCALE_FACTOR, -0.3 * SCALE_FACTOR] },
+  { name: "Procyon", position: [-0.068 * SCALE_FACTOR * X_STRETCH_FACTOR, 0.2 * SCALE_FACTOR, -0.7 * SCALE_FACTOR] },
 ];
 
 const connections = [
@@ -61,6 +61,9 @@ function Dots() {
   const [showLabels, setShowLabels] = useState(false);
   const [initialAngleSet, setInitialAngleSet] = useState(false);
 
+  // Define white star names
+  const whiteStarNames = ["Toonyne", "Kpyr", "NT-9", "Nyne", "Xiero", "Betelgeuse", "Phorr"];
+
   // Parse connections and prepare lines
   const lines = useMemo(() => {
     const parsedLines = [];
@@ -83,10 +86,12 @@ function Dots() {
     if (!initialAngleSet) {
       // Set a random initial camera position
       camera.position.set(
-        Math.random() * 5,
-        Math.random() * 5,
-        Math.random() * 5
+        Math.random() * 5 + 10,
+        Math.random() * 5 + 10,
+        Math.random() * 5 + 10,
       );
+      camera.zoom = 6;
+      camera.updateProjectionMatrix();
       setInitialAngleSet(true);
     }
 
@@ -104,8 +109,8 @@ function Dots() {
       // Define target angles and tolerances
       const targetAzimuthal = 0; // degrees
       const targetPolar = 90; // degrees
-      const azimuthalTolerance = 15; // degrees
-      const polarTolerance = 15; // degrees
+      const azimuthalTolerance = 25; // degrees
+      const polarTolerance = 25; // degrees
 
       // Condition to toggle labels
       if (
@@ -138,7 +143,7 @@ function Dots() {
         <React.Fragment key={i}>
           <mesh position={position}>
             <sphereGeometry args={[0.1, 32, 32]} />
-            <meshBasicMaterial color="#ffff6e" />
+            <meshBasicMaterial color={whiteStarNames.includes(name) ? "white" : "#ffff6e"} />
           </mesh>
           {showLabels && (
             <Text
@@ -160,6 +165,7 @@ function Dots() {
     </group>
   );
 }
+
 
 function App() {
   return (
